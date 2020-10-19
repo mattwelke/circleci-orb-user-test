5ef2d3ba00e4bcde987ba5ea0e2034082cc5fcf4
Matt Welke <mattwelke@gmail.com>
2020-10-19 17:08:50 -0400

    Add Node.js Pub/Sub test and docker command

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('Hello from the Node.js script.');
+const { PubSub } = require('@google-cloud/pubsub');
+
+const topicName = 'test-topic';
+const subName = 'test-subscription';
+
+async function waitMs(ms) {
+    return new Promise((accept, _) => {
+        setTimeout(accept, ms);
+    });
+};
+
+(async () => {
+    // Setup:
+    const pubSubClient = new PubSub();
+    console.log(`Created Pub/Sub client.`);
+
+    await pubSubClient.createTopic(topicName);
+    console.log(`Topic ${topicName} created.`);
+
+    const topic = pubSubClient.topic(topicName);
+
+    await topic.createSubscription(subName);
+    console.log(`Subscription ${subName} to topic ${topicName} created.`);
+
+    const sub = pubSubClient.subscription(subName);
+
+    // Test:
+    const testMsg = 'test-message';
+
+    let receivedExpected = false;
+
+    sub.addListener(rawMsg => {
+        const msg = Buffer.from(rawMsg, 'base64');
+        if (msg === testMsg) {
+            receivedExpected = true;
+        }
+    });
+
+    await topic.publish(Buffer.from(testMsg));
+
+    await waitMs(2000);
+
+    if (!receivedExpected) {
+        throw new Error('test failed because did not receive expected message from test subscription');
+    }
+})();
